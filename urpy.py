0622d13df49e807dc396406c4d053c38ebf2799b
Tobias Guggenmoser <t.guggenmoser@gmail.com>
2017-05-17 18:04:14 +0200

    some rearrangements

--- urpy.py
+++ urpy.py
@@ -7,7 +7,7 @@ import attr
 class IllegalMove(Exception): ...
 
 
-class Field(namedtuple('Field', ('war', 'rosette', 'go_again', 'token'))):
+class Field(namedtuple('Field', ('war', 'rosette', 'token'))):
 
     def __bool__(self):
         return self.token != Tokens.EMPTY
@@ -25,21 +25,24 @@ class Field(namedtuple('Field', ('war', 'rosette', 'go_again', 'token'))):
 class Tokens(Enum):
     EMPTY, P1, P2 = 0, 1, 2
 
+def opponent(player):
+    if player == Tokens.P1:
+        return Tokens.P2
+    elif player == Tokens.P2:
+        return Tokens.P1
+    else:
+        raise ValueError
 
 def can_move_to(player, field):
-    return field.empty() or (field.owned_by_opponent(player) and not field.rosette)
+    return field.empty() or (field.owned_by(opponent(player)) and not field.rosette)
 
 
 @attr.s
 class Track:
 
     tokens = attr.ib(default=[Tokens.EMPTY] * 14)
-    bench = attr.ib(default=7)
-    home = attr.ib(default=0)
-    bench_opp = attr.ib(default=7)
-    home_opp = attr.ib(default=0)
-    player = attr.ib(default=Tokens.P1)
-
+    bench = attr.ib(default={p: 7 for p in (Tokens.P1, Tokens.P2)})
+    home = attr.ib(default={p: 0 for p in (Tokens.P1, Tokens.P2)})
 
     def __len__(self):
         return len(self.tokens)
@@ -48,50 +51,32 @@ class Track:
         token = self.tokens[idx]
         return Field(war=(idx in range(4, 12)),
                      rosette=(idx in (3, 7, 13)),
-                     go_again=(idx == 3),
                      token=token)
 
     @property
-    def own_fields(self):
-        return (i for (i, fld) in enumerate(self) if fld.owned_by(self.player))
+    def owned_fields(self, player):
+        return (i for (i, fld) in enumerate(self) if fld.owned_by(player))
 
+    # def apply_move(self, player, mv):
 
-    def apply_move(self, player, mv):
-        if player == self.player:
-            if mv.src is None:
-                self.bench -= 1
-            if mv.dst is None:
-                self.home += 1
-            if self[mv.dst].owned_by_opponent(player):
-                self.bench_opp += 1
-                self.tokens[dst] = player
-        else:
-            if mv.src is None:
-                self.bench_opp -= 1
-            if mv.dst is None:
-                self.home_opp += 1
-            if self[mv.dst].owned_by_opponent(player):
-                self.bench += 1
-                self.tokens[dst] = player
 
 
 Move = namedtuple('Move', ('src', 'dst'))
 
 
-def get_moves(track, roll):
+def get_moves(track, player, roll):
     if roll == 0:
         return set()
 
-    pawns = {idx for idx in track.own_fields
+    pawns = {idx for idx in track.owned_fields(player)
              if idx + roll < len(track) + 1}
 
     remain = {idx for idx in pawns if idx + roll < len(track)}
     leave = pawns - remain
+    remain = {idx for idx in remain if can_move_to(player, track[idx + roll])}
+    can_enter = track.bench[player] > 0 and can_move_to(player, track[roll - 1])
 
-    remain_legal = {idx for idx in remain if can_move_to(track.player, track[idx + roll])}
-    can_enter = track.bench > 0 and can_move_to(track.player, track[roll - 1])
-
-    moves = {Move(src=idx, dst=idx + roll) for idx in remain_legal}
+    moves = {Move(src=idx, dst=idx + roll) for idx in remain}
     moves |= {Move(src=idx, dst=None) for idx in leave}
     if can_enter:
         moves.add(Move(src=None, dst=roll - 1))
@@ -100,5 +85,3 @@ def get_moves(track, roll):
 
 
 tr = Track()
-moves = get_moves(tr, 2)
-print(moves)
